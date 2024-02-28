ee2f719d408d496b28c8e8c9a56e31f35cabd7fc
samue <samuel.jdev11@gmail.com>
2024-02-28 15:50:20 -0400

    Usando anotações Lombok

--- src/main/java/org/example/model/Cliente.java
+++ src/main/java/org/example/model/Cliente.java
@@ -2,42 +2,17 @@ package org.example.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
 
-import java.util.Objects;
-
+@Getter
+@Setter
 @Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
     @Id
     private Integer id;
-
     private String nome;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cliente cliente = (Cliente) o;
-        return Objects.equals(id, cliente.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
