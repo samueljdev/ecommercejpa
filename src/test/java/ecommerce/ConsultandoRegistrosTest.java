54c37aadd75ee5fca90aa5690cc1c039164057a0
samue <samuel.jdev11@gmail.com>
2024-02-28 01:20:26 -0400

    metodo atualizar A Referencia

--- src/test/java/ecommerce/ConsultandoRegistrosTest.java
+++ src/test/java/ecommerce/ConsultandoRegistrosTest.java
@@ -35,6 +35,7 @@ public class ConsultandoRegistrosTest {
     @Test
     public void busarPorIdentificador() {
         Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(">>>>> Sempre faz o SQL");
         Assert.assertNotNull(produto);
         Assert.assertEquals("Kindle", produto.getNome());
     }
@@ -42,6 +43,19 @@ public class ConsultandoRegistrosTest {
     @Test
     public void busarPorGetReference() {
         Produto produto = entityManager.getReference(Produto.class, 1);
+        System.out.println(">>>>> Ainda não fez o SQL");
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+        System.out.println(">>>>> Já fez o SQL");
+    }
+
+    @Test
+    public void atualizarAReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+
+        entityManager.refresh(produto);
+
         Assert.assertNotNull(produto);
         Assert.assertEquals("Kindle", produto.getNome());
     }
