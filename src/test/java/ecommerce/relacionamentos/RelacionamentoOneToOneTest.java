92a8e6cfb91d97383951895788593bc0f1c3aaa0
samue <samuel.jdev11@gmail.com>
2024-03-02 18:10:39 -0400

    teste de relacionamento um para um

--- src/test/java/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
+++ src/test/java/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
@@ -1,11 +1,14 @@
 package ecommerce.relacionamentos;
 
 import ecommerce.EntityManagerTest;
+import org.example.model.NotaFiscal;
 import org.example.model.PagamentoCartao;
 import org.example.model.Pedido;
 import org.example.model.StatusPagamento;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Date;
 
 public class RelacionamentoOneToOneTest extends EntityManagerTest {
     @Test
@@ -25,5 +28,24 @@ public class RelacionamentoOneToOneTest extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         Assert.assertNotNull(pedidoVerificacao.getPagamentoCartao());
+    }
+
+    @Test
+    public void verificarRelacionamentoPedidoNotaFiscal() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setXml("TESTE");
+        notaFiscal.setDataEmissao(new Date());
+        notaFiscal.setPedido(pedido);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getNotafiscal());
     }
 }
