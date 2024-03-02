7aec451a85e8de5e94e52800bca51e1ccc631ae4
samue <samuel.jdev11@gmail.com>
2024-03-01 23:06:10 -0400

    remover Entidade Relacionada

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -3,3 +3,9 @@ insert into tb_produto (id, nome, preco, descricao) values (default, 'Câmera Go
 
 insert into tb_cliente (id, nome, sexo) values (default, 'Fernanda Fernandez', 'FEMININO');
 insert into tb_cliente (id, nome, sexo) values (default, 'Fernanda Lemos', 'FEMININO');
+
+insert into tb_pedido (id, cliente_id, data_pedido, total, status) values (default, 1, sysdate(), 100.0, 'AGUARDANDO');
+
+insert into tb_item_pedido (id, pedido_id, produto_id, preco_produto, quantidade) values (default, 1, 1, 5.0, 2);
+
+insert into tb_categoria (id, nome) values (default, 'Eletrônicos');
---
+++ src/test/java/ecommerce/relacionamentos/RemovendoEntidadesReferenciadasTest.java
@@ -0,0 +1,24 @@
+package ecommerce.relacionamentos;
+
+import ecommerce.EntityManagerTest;
+import org.example.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemovendoEntidadesReferenciadasTest extends EntityManagerTest {
+    @Test
+    public void removerEntidadeRelacionada() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+        entityManager.getTransaction().begin();
+        pedido.getItens().forEach(i -> entityManager.remove(i));
+        entityManager.remove(pedido);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, 1);
+        Assert.assertNull(pedidoVerificacao);
+    }
+}
