07c947ccb7b19909bbbb2514bd1e45ec65cca13b
samue <samuel.jdev11@gmail.com>
2024-03-01 22:53:32 -0400

    corrindo metodos

--- src/test/java/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -39,4 +39,33 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
         Assert.assertFalse(clienteVerificacao.getPedidos().isEmpty());
     }
 
+    @Test
+    public void verificarRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        Assert.assertNotNull(itemPedidoVerificacao.getPedido());
+        Assert.assertNotNull(itemPedidoVerificacao.getProduto());
+    }
+
 }
